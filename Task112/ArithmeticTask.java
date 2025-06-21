c3928d3ef2a88c52bae661408a46babbbc61e72a
Masamune <133774114+mazamunexd@users.noreply.github.com>
2025-06-21 03:25:24 +0000

    initial commit

--- Task112/ArithmeticTask.java
+++ Task112/ArithmeticTask.java
@@ -4,8 +4,10 @@ public class ArithmeticTask {
 
     public static void main(String[] args) {
 
-        int a = 10;
+        int a = 2;
         int b = 5;
+        int c = 7;
+        int d = 2;
 
         // Add
         int sum = a + b;
@@ -37,6 +39,41 @@ public class ArithmeticTask {
         int modulus = a % b; 
             System.out.println("Remainder: " + modulus);
 
-    }
+        // Shift operator
+        int shift = c >> a;
+            System.out.println("Shift >>: " + shift);
 
+        int shift2 = c << a;
+            System.out.println("Shift <<: " + shift2);
+
+        // Less than 
+        if (b < a) {
+            System.out.println( b + " is less than " + a);
+        } else {
+            System.out.println("It is not less than " + a + ".");
+        }
+        // Greater than 
+        if (b < c) {
+            System.out.println( b + " is greater than " + a);
+        } else {
+            System.out.println("It is not greater than " + a + ".");
+        }
+
+        // Equal to 
+        if (a == b) {
+            System.out.println(a + " is equal to " + b);
+        } else if (a == c) {
+            System.out.println(a + " is equal to " + c);
+        } else if (a == d) {
+            System.out.println(a + " is equal to " + d);
+        } else {
+            System.out.println(a + " is not equal to any.");
+        }
+
+        // Ternary 
+        int max = (a > b) ? a : b;
+        System.out.println("Maximum No.: " + max);
+        int min = (a < b) ? a : b;
+        System.out.println("Minimum No.: " + min);
+    }
 }
