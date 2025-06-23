e315059fe198f0b92a2d1c22cc65f825345c1f7f
Masamune <133774114+mazamunexd@users.noreply.github.com>
2025-06-23 03:47:50 +0000

    print H3110 w0r1d 2.0 true

---
+++ Task2/Task2.java
@@ -0,0 +1,43 @@
+package Task2;
+
+public class Task2 {
+    public static void main(String[] args) {
+
+        // H3110 w0r1d 2.0 true
+        
+        char charH = 'H';
+
+        char charW = 'W';
+
+        char charR = 'R';
+
+        char charD = 'D';
+
+        byte byteThree = 3;
+
+        short shortOne = 1;
+
+        int intZero = 0;
+
+        float floatValue = 2.0f;
+
+        boolean booleanValue = true;
+
+        String outputString = charH +
+                          String.valueOf(byteThree) +
+                          String.valueOf(shortOne) +
+                          String.valueOf(shortOne) +
+                          String.valueOf(intZero) +
+                          " " +
+                          String.valueOf(charW).toLowerCase() +
+                          String.valueOf(intZero) +
+                          String.valueOf(charR).toLowerCase() +
+                          String.valueOf(shortOne) +
+                          String.valueOf(charD).toLowerCase() + " " +
+                          String.valueOf(floatValue) +
+                          " " +
+                          String.valueOf(booleanValue);
+
+        System.out.println(outputString);
+    }
+}
