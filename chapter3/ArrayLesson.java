54e704c61b3f4660dd8601a427ce5da39d3ed837
Masamune <133774114+mazamunexd@users.noreply.github.com>
2025-07-01 03:19:56 +0000

    add lines in the lesson file

--- chapter3/ArrayLesson.java
+++ chapter3/ArrayLesson.java
@@ -2,35 +2,65 @@ package chapter3;
 
 import java.util.Arrays;
 
+/**
+ * Demonstrates basic array operations in Java.
+ */
 public class ArrayLesson {
-    public static void main(String...  args  ) {
-        int [] i = new int[3];
-        int[] numbers2 = new int[]{1,54,75}; 
-        int[] numbers3 = {1,54,75};
-        Student[] batch2 = {new Student(),new Student(),new Student(),new Student(),null};
-        for(int j = 0; j<numbers3.length;j++)
+    public static void main(String[] args) {
+        // 1. Declaring and initializing arrays
+        int[] emptyArray = new int[3]; // Array of 3 integers, all initialized to 0
+        int[] numbers2 = new int[]{1, 54, 75}; // Explicit initialization
+        int[] numbers3 = {1, 54, 75}; // Shorthand initialization
+
+        // 2. Array of objects
+        Student[] batch2 = {
+            new Student(), new Student(), new Student(), new Student(), null
+        };
+
+        // 3. Iterating through an array using a for loop
+        System.out.println("numbers3 contents:");
+        for (int j = 0; j < numbers3.length; j++) {
             System.out.println(numbers3[j]);
-        int[] numbers4 = {1,54,75,5,76,34,7,3,8,10,34};
+        }
+
+        // 4. Sorting and searching in arrays
+        int[] numbers4 = {1, 54, 75, 5, 76, 34, 7, 3, 8, 10, 34};
         Arrays.sort(numbers4);
-        for(int number4: numbers4)
-            System.out.print(number4+", ");
+        System.out.println("\nnumbers4 sorted:");
+        for (int number : numbers4) {
+            System.out.print(number + ", ");
+        }
+
         int searchNum = 9;
-        System.out.println("\n"+searchNum+" search result: "+ Arrays.binarySearch(numbers4, searchNum));
-        String[] numbersString = {"1","a","54","75","5","76","34","7","3","8","10","34"};
+        int searchResult = Arrays.binarySearch(numbers4, searchNum);
+        System.out.println("\nSearch for " + searchNum + ": " + searchResult);
+
+        // 5. String array sorting and searching
+        String[] numbersString = {"1", "a", "54", "75", "5", "76", "34", "7", "3", "8", "10", "34"};
         String stringSearch = "A";
         Arrays.sort(numbersString);
-        for(String numberString: numbersString)
-            System.out.print(numberString+", ");
-        System.out.println("\n"+stringSearch+" search result: "+ Arrays.binarySearch(numbersString, stringSearch));
-        int[][] diffsize = {{1,2},{3},{9,8,7}};
-        System.out.println(diffsize[1][2]);
-}
-//  1  2  3 4 5 6
- //  0  1  2 3 4 5
- //-1 -2 -3
+        System.out.println("\nnumbersString sorted:");
+        for (String s : numbersString) {
+            System.out.print(s + ", ");
+        }
+        int stringSearchResult = Arrays.binarySearch(numbersString, stringSearch);
+        System.out.println("\nSearch for \"" + stringSearch + "\": " + stringSearchResult);
+
+        // 6. Multidimensional arrays (jagged arrays)
+        int[][] diffsize = {
+            {1, 2},
+            {3},
+            {9, 8, 7}
+        };
+        // Accessing elements in a jagged array
+        System.out.println("\ndiffsize[2][2]: " + diffsize[2][2]); // prints 7
+    }
 }
 
-class Student{
+/**
+ * Simple Student class for demonstration.
+ */
+class Student {
     String fullname;
     int age;
     String address;
