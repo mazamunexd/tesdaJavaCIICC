c983959d400ca146895361dfb31cd80e57c2e5b7
Masamune <133774114+mazamunexd@users.noreply.github.com>
2025-07-02 03:53:40 +0000

    initial commit

---
+++ otherFolder/a/test.java
@@ -0,0 +1,32 @@
+package otherFolder.a;
+import java.util.*;
+
+public class test{
+  public static void main(String[] args) {
+    ArrayList list1 = new ArrayList();
+    list1.add(1);
+    list1.add('c');
+    list1.add(true);
+    list1.add("jav");
+    System.out.println(list1);
+
+    ArrayList list2 = new ArrayList(10);
+    ArrayList list3 = new ArrayList(list2);
+
+    ArrayList<String> list4 = new ArrayList<String>();
+    ArrayList<String> list5 = new ArrayList<>();
+
+    ArrayList<Integer> list6 = new ArrayList<>();
+    list6.add(21);
+    list6.add(11);
+    list6.add(3);
+    list6.add(19);
+    list6.add(9);
+    list6.add(0, 100);
+    System.out.println(list6.add(9));
+    System.out.println(list6);
+
+
+
+  }
+}
