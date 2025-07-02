f9988b47469ab174c10cd27598ab09eee5bd4070
Masamune <133774114+mazamunexd@users.noreply.github.com>
2025-07-02 06:19:07 +0000

    initial commit

--- Task5/Task5.java
+++ Task5/Task5.java
@@ -6,24 +6,33 @@ public class Task5 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println();
         System.out.print("Enter first number: ");
-        double num1 = scanner.nextDouble();
+        int num1 = scanner.nextInt();
         System.out.print("Enter second number: ");
-        double num2 = scanner.nextDouble();
+        int num2 = scanner.nextInt();
         System.out.print("Enter third number: ");
-        double num3 = scanner.nextDouble();
+        int num3 = scanner.nextInt();
 
         if (num1 == num2 && num2 == num3) {
-            System.out.println("All numbers are equal");
+            System.out.println();
+            System.out.println("=========================");
+            System.out.println("| All numbers are equal |");
+            System.out.println("=========================");
+            System.out.println();
         } else {
-            double largest = num1;
+            int largest = num1;
             if (num2 > largest) {
                 largest = num2;
             }
             if (num3 > largest) {
                 largest = num3;
             }
-            System.out.println("Largest number: " + largest);
+            System.out.println();
+            System.out.println("================================");
+            System.out.printf("| Largest number: %-12s |\n", largest);
+            System.out.println("================================");
+            System.out.println();
         }
 
         scanner.close();
